679255329fe1afe6fc250b5187fd3535f5a27d10
Abel Brencsan <abel@brencsan.hu>
2024-01-18 23:04:29 +0100

    Remove compiled script

--- ui/assets/js/scripts.js
+++
@@ -1,2 +0,0 @@
-class App{constructor(){this.pages=new Pages();this.pages.push(new DeviceSelectorPage("device-selector-page"));this.pages.push(new DeviceSelectorPage("device-pairing-page"));this.pages.showById("device-selector-page");}}
-const app=new App();
