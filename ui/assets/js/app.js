892be0e87368be63e069cdb2e23c1e9ae6435348
Abel Brencsan <abel@brencsan.hu>
2024-01-25 18:30:46 +0100

    Rename pages class to page list

--- ui/assets/js/app.js
+++ ui/assets/js/app.js
@@ -1,11 +1,11 @@
 import { DeviceSelectorPage } from "./page/device-selector-page.js";
 import { DevicePairingPage } from "./page/device-pairing-page.js";
-import { Pages } from "./pages.js";
+import { PageList } from "./page-list.js";
 import { Page } from "./page.js";
 
 class App {
     constructor() {
-        this.pages = new Pages();
+        this.pages = new PageList();
         this.pages.push(new DeviceSelectorPage("device-selector-page"));
         this.pages.push(new DevicePairingPage("device-pairing-page"));
         this.pages.showById("device-selector-page");
--- ui/assets/js/page-list.js
+++ ui/assets/js/page-list.js
@@ -1,4 +1,4 @@
-class Pages {
+class PageList {
     constructor() {
         this.pages = [];
     }
@@ -17,4 +17,4 @@ class Pages {
     }
 }
 
-export { Pages };
+export { PageList };
