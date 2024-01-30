90b14a373f02420971c40cb339bfe4153738a252
David Brencsan <davidbrencsan@gmail.com>
2024-01-30 15:17:26 +0100

    Implements automatic window resizing at program starts

--- calypso_go/__init__.py
+++ calypso_go/__init__.py
@@ -1,6 +1,45 @@
 import webview
+from screeninfo import get_monitors
+from typing import Tuple
+
+class Screen:
+    def __init__(self) -> None:
+        pass
+
+    def get_screen_size_percentage(self, percentage_width: int, percentage_height: int) -> tuple[int,int]:
+
+        '''
+        The method calculates the current display width and height in pixels based on the percentage values provided in the parameters.
+
+        Parameters:
+            percentage_width(int): Percentage of the active display's width
+            percentage_height(int): Percentage of the active display's height
+
+        Returns:
+            Tuple[int, int]: It returns the current display width and height in pixels.
+        '''
+
+        monitors = get_monitors()
+        if monitors:
+            primary_monitor = monitors[0]
+            width_percentage = int(primary_monitor.width * percentage_width / 100)
+            height_percentage = int(primary_monitor.height * percentage_height / 100)
+            return width_percentage, height_percentage
+        else:
+            return 800, 600
 
 
 def run():
-    webview.create_window("Calypso Go", "ui/app.html")
+
+    screen = Screen()
+    width, height = screen.get_screen_size_percentage(40,70)
+
+    webview.create_window(
+        "Calypso Go",
+        "ui/app.html",
+        width = width,
+        height = height,
+        min_size=(400, 300)
+    )
+    
     webview.start(debug=True)
