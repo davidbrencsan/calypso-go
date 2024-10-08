1f746cfffef8c6f04de8255e25b445442888ca02
David Brencsan <davidbrencsan@gmail.com>
2024-10-08 14:51:19 +0200

    change logic

--- calypso_go/__init__.py
+++ calypso_go/__init__.py
@@ -1,7 +1,50 @@
 import webview
+import asyncio
 from .model import Screen
+from bleak import BleakScanner
+from bleak.backends.device import BLEDevice
+
+class JsApi:
+
+    def __init__(self) -> None:
+        pass
+
+    async def explore_devices(self) -> list[BLEDevice]:
+        """"""
+        return await BleakScanner.discover()
+
+    def get_device_list(self, timeout: float = 5) -> list:
+        """"""
+
+        def ble_device_to_dict(item:BLEDevice) -> dict:
+            return {
+                "address": item.address,
+                "name": item.name
+            }
+
+        res = asyncio.run(self.explore_devices())
+        results = list(map(ble_device_to_dict, res))
+        return results
+    
+    def echo(self):
+        return "Hello"
+    
+    def create_test_json(self) -> dict:
+        return {
+            "address": "AA:01:04:4F:61:66",
+            "name": "Ultrasonic"
+        }
+    
+    # def create_html_template(self, testdata:dict):
+    #     print("Hello")
 
 def run():
+
+    js_api = JsApi()
+
+    test_data = js_api.create_test_json()
+    print(test_data)
+
 
     screen = Screen()
     width, height = screen.get_screen_size_percentage(40,70)
@@ -9,9 +52,10 @@ def run():
     webview.create_window(
         "Calypso Go",
         "ui/app.html",
-        width = width,
-        height = height,
-        min_size=(400, 300)
+        width=width,
+        height=height,
+        min_size=(400, 300),
+        js_api=js_api,
     )
     
     webview.start(debug=True)
